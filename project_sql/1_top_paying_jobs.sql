103a7d85e7315343f4fc9736015a18cfe1f1ac9e
Nhido1105 <donhi345vta@gmail.com>
2025-10-08 21:04:01 +0700

    project sql

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -9,8 +9,6 @@ SELECT job_id,
 FROM job_postings_fact
     LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
 WHERE job_title_short = 'Data Analyst'
-    AND salary_year_avg > 60000
     AND salary_year_avg IS NOT NULL
-    AND job_work_from_home = 'TRUE'
 ORDER BY salary_year_avg DESC
 LIMIT 10;
--- project_sql/2_top_paying_jobs_skills.sql
+++ project_sql/2_top_paying_jobs_skills.sql
@@ -10,7 +10,6 @@ WITH tpj AS (
         LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
     WHERE job_title_short = 'Data Analyst'
         AND salary_year_avg IS NOT NULL
-        AND job_work_from_home = 'TRUE'
     ORDER BY salary_year_avg DESC
     LIMIT 10
 )
